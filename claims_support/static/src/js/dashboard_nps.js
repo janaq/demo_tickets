c9f471cde7e69feef3e844e15b4cb45fbb52d213
Kimberly Jiménez <kimberly.jimenez@janaq.com>
2024-10-23 10:25:45 -0500

    [FIX] claims_support: Definición de la plantilla con las secciones de los gráficos para el reporte

--- claims_support/static/src/js/dashboard_nps.js
+++ claims_support/static/src/js/dashboard_nps.js
@@ -22,19 +22,17 @@ export class DashboardNPS extends Component{
             await loadJS("/web/static/lib/Chart/Chart.js"); // [ lib: CHART ]
             await loadJS("claims_support/static/lib/jszip.min.js"); // [ lib: ZIP ]
             await loadJS("claims_support/static/lib/FileSaver.min.js"); // [lib: saveAs ]
+            this.informationBrand = await this.orm.searchRead("claim.config", [], ["id","name","store_ids"]) 
             this.informationShop = await this.orm.searchRead("helpdesk.tienda", [], ["id","name"])
-            this.informationBrand = await this.orm.searchRead("claim.config", [], ["id","name","store_ids"])            
+                       
         })
         onMounted(()=> {
             this.informationSelectsDashboard(); // [RENDERIZAR SELECT]
-            this.render_dashboards(); // [RENDERIZAR CANVAS]
-            this.daterangepicker_dashboard(); // [DATERANGEPICKER]
+            this.daterangepickerDashboard(); // [DATERANGEPICKER]
+            this.renderDashboard(); // [RENDERIZAR CANVAS]
         })
         useExternalListener(document.body, "click", (ev) => this.dowloadButtonImage(ev) );
     }
-
-
-
 
     // [ INCLUIR OPCIONES AL SELECT DE ENCUESTAS ]
     informationSelectsDashboard() {
@@ -44,23 +42,109 @@ export class DashboardNPS extends Component{
             $options = $('<option/>',{text:value['name'],value:value['id']});
             $('#sltBrand').prepend($options);
         });
+        $.each(self.informationShop, function( index, value ) {
+            $options = $('<option/>',{text:value['name'],value:value['id']});
+            $('#sltShop').prepend($options);
+        });
+    }
+
+    // [ FUNCIONALIDAD PARA EL DATERANGEPICKER ]
+    daterangepickerDashboard () {
+        $(function() {
+            let start = moment().subtract(6, 'days');
+            let end = moment();
+            function cb(start, end) {
+                $('#divRange span').html(start.format('D MMMM, YYYY') + ' - ' + end.format('D MMMM, YYYY'));
+                $("#divRange").prop("rangeStart",start.format('YYYY-MM-DD'));
+                $("#divRange").prop("rangEnd",end.format('YYYY-MM-DD'));
+                $('#statistics').click()
+            }
+            $('#divRange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    'Hoy': [moment(), moment()],
+                    'Ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Últimos 7 días': [moment().subtract(6, 'days'), moment()],
+                    'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
+                    'Este mes': [moment().startOf('month'), moment().endOf('month')],
+                    'Mes pasado': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, cb);
+            cb(start, end);
+        });
     }
     
     //[ RENDERIZAR TEMPLATES DE DASHBOARD ]
-    render_dashboards() {}
-
-    // [ FUNCIONALIDAD PARA EL DATERANGEPICKER ]
-    daterangepicker_dashboard () {}
-
-    // [ GRAFICAR CONSULTAS NPS EN CANVAS: FUNCIÓN PRINCIPAL ]
-    async build_graphs(){}
-
-    //[ DESCARGA DE LAS GRÁFICAS EN CANVAS ]
-    downloadImages(){
+    renderDashboard() {
+        let self = this;
+        let tmps = ['NetPromoterScore','SurveyParticipation','NPSBreakdown','NPSValue'];
+        let tmps_1 = ['NPStore'];
+        _.each(tmps, function(tmp) { $('.o_hr_dashboard').append(QWeb.render(tmp, {widget: self})); });
+        _.each(tmps_1, function(tmp) { $('.o_hr_dashboard_general').append(QWeb.render(tmp, {widget: self})); });
     }
 
+    // [ GRAFICAR CONSULTAS NPS EN CANVAS: FUNCIÓN PRINCIPAL ]
+    async buildGraphs(){
+        console.log('OPERACIONES PARA LA VISUALIZACIÓN DE LOS CÁLCULOS')
+    }
 
+    // [ DESCARGA GRUPAL DE LAS GRÁFICAS EN FORMATO ZIP ]
+    downloadImages(){
+        let zip = new JSZip();
+        let d = new Date();
+        zip.file("NPS SCORE NETO - "+d+".png", graphScoreNeto.toBase64Image().replace('data:image/png;base64,',''), {base64: true});
+        zip.file("PARTICIPACIÓN EN LAS ENCUESTAS - "+d+".png", graphSurveyParticipation.toBase64Image().replace('data:image/png;base64,',''), {base64: true});
+        zip.file("NPS BREAKDOWN - "+d+".png", graphBreakdown.toBase64Image().replace('data:image/png;base64,',''), {base64: true});
+        zip.file("NPS VALUE - "+d+".png", graphValue.toBase64Image().replace('data:image/png;base64,',''), {base64: true});
+        zip.file("NPS STORE - "+d+".png", graphStore.toBase64Image().replace('data:image/png;base64,',''), {base64: true});
+        zip.generateAsync({type:"blob"}).then(function(content) {   saveAs(content, "Dashboard NPS "+d+".zip"); });
+    }
+    // [ DESCARGA INDIVIDUAL EN FORMATO PNG ]
+    dowloadButtonImage(ev){
+        let self = this
+        if (ev.target.id == 'downloadBtnNetPromoterScore'){ self.downloadBtnNetPromoterScore() }
+        if (ev.target.id == 'downloadBtnSurveyParticipation') { self.downloadBtnSurveyParticipation() }
+        if (ev.target.id == 'downloadBtnNPSBreakdown') { self.downloadBtnNPSBreakdown() }
+        if (ev.target.id == 'downloadBtnNPSValue') { self.downloadBtnNPSValue()}
+        if (ev.target.id == 'downloadBtnNPStore') { self.downloadBtnNPStore()}
 
+    }
+    downloadBtnNetPromoterScore(){
+        let d = new Date();
+        let a = document.createElement('a');
+        a.href = graphScoreNeto.toBase64Image();
+        a.download = 'NPS SCORE NETO '+ d +'.png' ;
+        a.click();
+    }
+    downloadBtnSurveyParticipation(){
+        let d = new Date();
+        let a = document.createElement('a');
+        a.href = graphSurveyParticipation.toBase64Image();
+        a.download = 'PARTICIPACIÓN EN LAS ENCUESTAS '+ d +'.png' ;
+        a.click();
+    }
+    downloadBtnNPSBreakdown(){
+        let d = new Date();
+        let a = document.createElement('a');
+        a.href = graphBreakdown.toBase64Image();
+        a.download = 'NPS BREAKDOWN '+ d +'.png' ;
+        a.click();
+    }
+    downloadBtnNPSValue(){
+        let d = new Date();
+        let a = document.createElement('a');
+        a.href = graphValue.toBase64Image();
+        a.download = 'NPS VALUE '+ d +'.png' ;
+        a.click();
+    }
+    downloadBtnNPStore(){
+        let d = new Date();
+        let a = document.createElement('a');
+        a.href = graphStore.toBase64Image();
+        a.download = 'NPS VALUE '+ d +'.png' ;
+        a.click();
+    }
 
 }
 
